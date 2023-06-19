14ae124b59f54e66c31e24ff183b16693c203165
Junimeek <secjunipj39@protonmail.com>
2023-06-19 15:59:06 -0500

    23w25b

--- Assets/Scripts/Assembly-CSharp/OptionsManager.cs
+++ Assets/Scripts/Assembly-CSharp/OptionsManager.cs
@@ -11,89 +11,74 @@ public class OptionsManager : MonoBehaviour
 	{
 		if (PlayerPrefs.HasKey("OptionsSet"))
 		{
-			this.slider.value = PlayerPrefs.GetFloat("MouseSensitivity");
-			if (PlayerPrefs.GetInt("Rumble") == 1)
-			{
-				this.rumble.isOn = true;
-			}
-			else
-			{
-				this.rumble.isOn = false;
-			}
-			if (PlayerPrefs.GetInt("AnalogMove") == 1)
-			{
-				this.analog.isOn = true;
-			}
-			else
-			{
-				this.analog.isOn = false;
-			}
-			if (PlayerPrefs.GetInt("InstantReset") == 1)
-			{
-				this.instantReset.isOn = true;
-			}
-			else
-			{
-				this.instantReset.isOn = false;
-			}
-			if (PlayerPrefs.GetInt("AdditionalMusic") == 1)
-			{
-				this.additionalMusic.isOn = true;
-			}
-			else
-			{
-				this.additionalMusic.isOn = false;
-			}
+			this.sensitivitySlider.value = PlayerPrefs.GetFloat("MouseSensitivity");
+			this.voiceSlider.value = PlayerPrefs.GetFloat("VolumeVoice");
+			this.bgmSlider.value = PlayerPrefs.GetFloat("VolumeBGM");
+
+			if (PlayerPrefs.GetInt("Rumble") == 1) this.rumble.isOn = true;
+			else this.rumble.isOn = false;
+			
+			if (PlayerPrefs.GetInt("AnalogMove") == 1) this.analog.isOn = true;
+			else this.analog.isOn = false;
+
+			if (PlayerPrefs.GetInt("InstantReset") == 1) this.instantReset.isOn = true;
+			else this.instantReset.isOn = false;
+
+			if (PlayerPrefs.GetInt("AdditionalMusic") == 1) this.additionalMusic.isOn = true;
+			else this.additionalMusic.isOn = false;
+
+			if (PlayerPrefs.GetInt("NotifBoard") == 1) this.notifToggle.isOn = true;
+			else this.notifToggle.isOn = false;
 		}
 		else
 		{
 			PlayerPrefs.SetInt("OptionsSet", 1);
+			PlayerPrefs.SetInt("Rumble", 0);
+			PlayerPrefs.SetInt("AnalogMove", 0);
+			PlayerPrefs.SetInt("InstantReset", 0);
+			PlayerPrefs.SetInt("AdditionalMusic", 0);
+			PlayerPrefs.SetInt("NotifBoard", 1);
+			PlayerPrefs.SetFloat("MouseSensitivity", 0.2f);
+			PlayerPrefs.SetFloat("VolumeVoice", 0.9f);
+			PlayerPrefs.SetFloat("VolumeBGM", 0.9f);
+			PlayerPrefs.SetFloat("VolumeSFX", 0.9f);
 		}
 	}
 
 	// Token: 0x06000062 RID: 98 RVA: 0x00003850 File Offset: 0x00001C50
 	private void Update()
 	{
-		PlayerPrefs.SetFloat("MouseSensitivity", this.slider.value);
-		if (this.rumble.isOn)
-		{
-			PlayerPrefs.SetInt("Rumble", 1);
-		}
-		else
-		{
-			PlayerPrefs.SetInt("Rumble", 0);
-		}
-		if (this.analog.isOn)
-		{
-			PlayerPrefs.SetInt("AnalogMove", 1);
-		}
-		else
-		{
-			PlayerPrefs.SetInt("AnalogMove", 0);
-		}
-		if (this.instantReset.isOn)
-		{
-			PlayerPrefs.SetInt("InstantReset", 1);
-		}
-		else
-		{
-			PlayerPrefs.SetInt("InstantReset", 0);
-		}
-		if (this.additionalMusic.isOn)
-		{
-			PlayerPrefs.SetInt("AdditionalMusic", 1);
-		}
-		else
-		{
-			PlayerPrefs.SetInt("AdditionalMusic", 0);
-		}
+		PlayerPrefs.SetFloat("MouseSensitivity", this.sensitivitySlider.value);
+		PlayerPrefs.SetFloat("VolumeVoice", this.voiceSlider.value);
+		PlayerPrefs.SetFloat("VolumeBGM", this.bgmSlider.value);
 
-		this.sensitivityPercent = (int)(slider.value*10);
-		this.text.text = sensitivityPercent.ToString() + "%";
+		if (this.rumble.isOn) PlayerPrefs.SetInt("Rumble", 1);
+		else PlayerPrefs.SetInt("Rumble", 0);
+
+		if (this.analog.isOn) PlayerPrefs.SetInt("AnalogMove", 1);
+		else PlayerPrefs.SetInt("AnalogMove", 0);
+
+		if (this.instantReset.isOn) PlayerPrefs.SetInt("InstantReset", 1);
+		else PlayerPrefs.SetInt("InstantReset", 0);
+
+		if (this.additionalMusic.isOn) PlayerPrefs.SetInt("AdditionalMusic", 1);
+		else PlayerPrefs.SetInt("AdditionalMusic", 0);
+
+		if (this.notifToggle.isOn) PlayerPrefs.SetInt("NotifBoard", 1);
+		else PlayerPrefs.SetInt("NotifBoard", 0);
+
+		this.sensitivityPercent = (int)(sensitivitySlider.value*10);
+		this.voiceVolume = (int)(voiceSlider.value*100);
+		this.bgmVolume = (int)(bgmSlider.value*100);
+		this.sensitivityText.text = sensitivityPercent.ToString() + "%";
+		this.voiceText.text = voiceVolume.ToString() + "%";
+		this.bgmText.text = bgmVolume.ToString() + "%";
 	}
 
 	// Token: 0x0400006F RID: 111
-	public Slider slider;
+	public Slider sensitivitySlider;
+	public Slider voiceSlider;
+	public Slider bgmSlider;
 
 	// Token: 0x04000070 RID: 112
 	public Toggle rumble;
@@ -103,7 +88,12 @@ public class OptionsManager : MonoBehaviour
 
 	public Toggle instantReset;
 	public Toggle additionalMusic;
+	public Toggle notifToggle;
 
 	[SerializeField] private float sensitivityPercent;
-	[SerializeField] private TMP_Text text;
+	[SerializeField] private TMP_Text sensitivityText;
+	[SerializeField] private float voiceVolume;
+	[SerializeField] private TMP_Text voiceText;
+	[SerializeField] private float bgmVolume;
+	[SerializeField] private TMP_Text bgmText;
 }
